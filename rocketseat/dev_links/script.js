21e742970473f9dc91f83496f76540237d2db797
Dimi <dimitrios007emerick@gmail.com>
2023-10-09 16:39:34 -0300

    Toggle Photos
    added image for the light theme

--- rocketseat/dev_links/script.js
+++ rocketseat/dev_links/script.js
@@ -1,3 +1,12 @@
 function toggleMode() {
+    // trocar a classe light do html
    document.documentElement.classList.toggle('light')
+
+   //substituir a imagem de acordo com o tema
+   const img = document.querySelector("#profile img")
+   if (document.documentElement.classList.contains('light')) {
+    img.setAttribute('src', './assets/avatar-light.png')
+   } else {
+    img.setAttribute('src', './assets/avatar.png')
+   }
 }
